63829ace61066a5660ffe578341ec1c8351d5c7e
BryanGod-end <strangebryan@outlook.com>
2026-07-26 08:10:52 -0500

    Menu movil como panel deslizante

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -16,15 +16,15 @@ export default function Navbar() {
         </Link>
 
         <div className="navbar-right">
-          <ul className={`nav-links ${menuOpen ? 'open' : ''}`}>
-            <li><Link to="/categoria/tecnologia" onClick={closeMenu}>Tecnología</Link></li>
-            <li><Link to="/categoria/maquillaje" onClick={closeMenu}>Maquillaje</Link></li>
-            <li><Link to="/categoria/hogar" onClick={closeMenu}>Hogar</Link></li>
-            <li><Link to="/categoria/cocina" onClick={closeMenu}>Cocina</Link></li>
-            <li><Link to="/categoria/juguetes" onClick={closeMenu}>Juguetes</Link></li>
+          <ul className="nav-links">
+            <li><Link to="/categoria/tecnologia">Tecnología</Link></li>
+            <li><Link to="/categoria/maquillaje">Maquillaje</Link></li>
+            <li><Link to="/categoria/hogar">Hogar</Link></li>
+            <li><Link to="/categoria/cocina">Cocina</Link></li>
+            <li><Link to="/categoria/juguetes">Juguetes</Link></li>
           </ul>
 
-          <Link to="/Carrito" className="cart-link" onClick={closeMenu}>
+          <Link to="/Carrito" className="cart-link">
             <img src="/images/Carrito.png" alt="Carrito" className="cart-icon" />
             {itemCount > 0 && <span className="cart-badge">{itemCount}</span>}
           </Link>
@@ -32,11 +32,27 @@ export default function Navbar() {
           <button
             className="menu-toggle"
             aria-label="Abrir menú"
-            onClick={() => setMenuOpen((open) => !open)}
+            onClick={() => setMenuOpen(true)}
           >
-            {menuOpen ? '✕' : '☰'}
+            ☰
           </button>
         </div>
+      </div>
+
+      <div className={`nav-overlay ${menuOpen ? 'open' : ''}`} onClick={closeMenu} />
+
+      <div className={`nav-drawer ${menuOpen ? 'open' : ''}`}>
+        <div className="nav-drawer-header">
+          <span>Menú</span>
+          <button className="nav-drawer-close" onClick={closeMenu} aria-label="Cerrar menú">✕</button>
+        </div>
+        <ul className="nav-drawer-links">
+          <li><Link to="/categoria/tecnologia" onClick={closeMenu}>Tecnología</Link></li>
+          <li><Link to="/categoria/maquillaje" onClick={closeMenu}>Maquillaje</Link></li>
+          <li><Link to="/categoria/hogar" onClick={closeMenu}>Hogar</Link></li>
+          <li><Link to="/categoria/cocina" onClick={closeMenu}>Cocina</Link></li>
+          <li><Link to="/categoria/juguetes" onClick={closeMenu}>Juguetes</Link></li>
+        </ul>
       </div>
     </header>
   );
